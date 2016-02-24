1a9e5e3846f1b587cbd97d6e76e65d0ee86f1eba
Julian Sparber <julian@sparber.net>
2016-02-24 13:54:53 +0100

    Add Graphices

---
+++ Chess/Graphics.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Gtk;
+
+namespace Chess
+{
+  public partial class Graphics : Gtk.Bin
+  {
+    private GridWidget mainGrid;
+    private SidebarWidget sidebarRight;
+    private SidebarWidget sidebarLeft;
+    public Graphics (Game g)
+    {
+      MainWindow win = new MainWindow ();
+      VBox gridWrapper = new VBox ();
+      HBox box = new HBox ();
+      //win.Resize (800, 800);
+      //win.Decorated = false;
+      this.mainGrid = new GridWidget (g, this);
+      gridWrapper.PackStart (new Label("") , false, false, 0);
+      gridWrapper.PackStart (this.mainGrid, false, false, 0);
+      this.sidebarLeft = new SidebarWidget (g.getRemovedFigures (), "black");
+      box.PackStart (new HBox ());
+      box.PackStart (this.sidebarLeft);
+      box.PackStart (gridWrapper, false, false, 0);
+      this.sidebarRight = new SidebarWidget (g.getRemovedFigures (), "white");
+      box.PackEnd (this.sidebarRight);
+      box.PackStart (new HBox ());
+      box.ShowAll ();
+      win.Add (box);
+      win.Show ();
+    }
+
+    public void updateGui() {
+      this.sidebarLeft.updateSidebar ();
+      this.sidebarRight.updateSidebar ();
+    }
+
+  }
+}
+
