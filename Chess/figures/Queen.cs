4167c12b39742dcca5b04f087f6cf7ec598f7003
Julian Sparber <julian@sparber.net>
2016-02-29 17:26:40 +0100

    [fix] fixed rules for rock [add] rulles for queen

--- Chess/figures/Queen.cs
+++ Chess/figures/Queen.cs
@@ -3,11 +3,76 @@ using System;
 namespace Chess
 {
   public class Queen : Figure
-	{
+  {
     public Queen (string color) : base (color)
     {
     }
-	}
+
+
+    public override bool move (Board board, coord start, coord end)
+    {
+      //bishop case
+      if (Math.Abs (start.x - end.x) == Math.Abs (start.y - end.y)) {
+        coord tmp = start;
+        while (!tmp.Equals (end)) {
+          if (board.getFieldFigure (tmp) != "Empty" && !tmp.Equals (start))
+            return false;
+          if (tmp.x > end.x && tmp.y > end.y) {
+            tmp.x--;
+            tmp.y--;
+          }
+          if (tmp.x > end.x && tmp.y < end.y) {
+            tmp.x--;
+            tmp.y++;
+          }
+          if (tmp.x < end.x && tmp.y > end.y) {
+            tmp.x++;
+            tmp.y--;
+          }
+          if (tmp.x < end.x && tmp.y < end.y) {
+            tmp.x++;
+            tmp.y++;
+          }
+        }
+        return true;
+      }
+
+      //if not bishop case its rock case
+      if (start.x == end.x) {
+        if (start.y < end.y) {
+          for (int i = start.y + 1; i < end.y; i++) {  
+            if (board.getFieldFigure (start.x, i) != "Empty") {
+              return false;
+            }
+          }
+        } else {
+          for (int i = start.y - 1; i > end.y; i--) {  
+            if (board.getFieldFigure (start.x, i) != "Empty") {
+              return false;
+            }
+          }
+        }
+        return true;
+      } else if (start.y == end.y) {
+        if (start.x < end.x) {
+          for (int i = start.x + 1; i < end.x; i++) {  
+            if (board.getFieldFigure (i, start.y) != "Empty") {
+              return false;
+            }
+          }
+        } else {
+          for (int i = start.x - 1; i > end.x; i--) {  
+            if (board.getFieldFigure (i, start.y) != "Empty") {
+              return false;
+            }
+          }
+        }
+        return true;
+      }  
+      return false;
+
+    }
+  }
 
 }
 
--- Chess/figures/Rock.cs
+++ Chess/figures/Rock.cs
@@ -3,7 +3,7 @@ using System;
 namespace Chess
 {
   public class Rock : Figure
-	{
+  {
     public Rock (string color) : base (color)
     {
       this.rule = new coord (1, 0);
@@ -11,8 +11,8 @@ namespace Chess
 
     public override bool move (Board board, coord start, coord end)
     {
-    //  int x = this.rule.x;
-     // int y = this.rule.y;
+      //  int x = this.rule.x;
+      // int y = this.rule.y;
 
       if (start.x == end.x) {
         if (start.y < end.y) {
@@ -23,28 +23,32 @@ namespace Chess
           }
         } else {
           for (int i = start.y - 1; i > end.y; i--) {  
-            if (board.getFieldFigure(start.x, i) != "Empty") {
+            if (board.getFieldFigure (start.x, i) != "Empty") {
               return false;
             }
           }
         }
-      }
-      else if (start.y == end.y) {
+        return true;
+      } else if (start.y == end.y) {
         if (start.x < end.x) {
           for (int i = start.x + 1; i < end.x; i++) {  
-            if (board.getFieldFigure(i, start.y) != "Empty") {
+            if (board.getFieldFigure (i, start.y) != "Empty") {
               return false;
             }
           }
         } else {
           for (int i = start.x - 1; i > end.x; i--) {  
-            if (board.getFieldFigure(i, start.y) != "Empty") {
+            if (board.getFieldFigure (i, start.y) != "Empty") {
               return false;
             }
           }
         }
+        return true;
       }  
-       return true;
-      }
-   }
+      return false;
+    }
+  
+      
+  }
+
 }
