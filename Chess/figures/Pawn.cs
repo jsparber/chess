209f0ae6bfda8f92e1ca4a1a58c7fa132c68ad12
fs <unknown-user@192.168.247.129>
2016-02-29 08:45:40 -0800

    + added movement of two tiles + added counter for seeing pawn move(not moved,single move or double move) for en passant

--- Chess/figures/Pawn.cs
+++ Chess/figures/Pawn.cs
@@ -4,10 +4,73 @@ namespace Chess
 {
   public class Pawn : Figure
 	{
+    private int count;
+    /* Constructor of the object pawn*/
     public Pawn (string color) : base(color)
     {
+      this.count = 0;     /* Count for checking if the pawn has moved */      
     }
+
+
     public override bool move(Board board, coord start, coord end) {
+      //TODO counter for movement; movement of 2 tiles
+      /* Counter TODO TODO TODO */
+     /* int count = 0;              
+      int doublemov = 0;          /* Count for checking if the pawn has done the doublemove */ 
+
+      if(this.count == 0){
+        /* Special movement of pawn: two tile ahead */
+
+        /* This method check the color of the figure, if it is white*/
+        /* the final position is equal to the initial position with an increment */
+        /* of two of the value y,else the final position is equal to the initial position */
+        /* with a decrement of two of the value y */
+
+        if (this.Color == "white") {    /* Check of the color of the figure */
+
+          /* If the final value of y is equal to the starting value plus two */
+          /* there is a control for the value x:if the final value is equal */
+          /* to the starting value the movement is possible */
+          if (start.y + 2 == end.y) {  
+            if (start.x == end.x) {
+              /* Setting count to two */
+              this.count = 2;
+
+              return true;
+            }
+          }
+
+          /* If the value of x and y are not corresponding to the rule the movement isn't allowed  */
+          return false;
+        } 
+        /* If the color is black we have a decrement of the value y */
+        else {
+
+          /* If the final value of y is equal to the starting value minus two */
+          /* there is a control for the value x:if the final value is equal */
+          /* to the starting value the movement is possible */
+          if (start.y - 2 == end.y) {
+            if (start.x == end.x) {
+              /* Setting count to two */
+              this.count = 2;
+
+              return true;
+            }
+          }
+
+          /* Setting count to two */
+          this.count = 2;
+
+          /* If the value of x and y are not corresponding to the rule the movement isn't allowed  */
+          return false;
+
+      }
+        /*Setting doublemov to one 
+        doublemov = 1;*/
+
+      
+      }
+        
       /* Basic movement of pawn: one tile ahead */
 
       /* This method check the color of the figure, if it is white*/
@@ -22,7 +85,12 @@ namespace Chess
         /* to the starting value the movement is possible */
         if (start.y + 1 == end.y) {   
           if (start.x == end.x) {
+            /* If count is zero it will be set to one */        
+            if(this.count == 0){             
+              this.count = 1;
+            }
             return true;
+
           }
         }
 
@@ -37,14 +105,22 @@ namespace Chess
         /* to the starting value the movement is possible */
         if (start.y - 1 == end.y) {
           if (start.x == end.x) {
+            /* If count is zero it will be set to one */        
+            if (this.count == 0) {             
+              this.count = 1;
+            }
             return true;
+
           }
+        }
+          /* If count is zero it will be set to one */        
+        if (this.count == 0) {             
+          this.count = 1;
         }
 
         /* If the value of x and y are not corresponding to the rule the movement isn't allowed  */
         return false;
-      }
+      }         
     }
 	}
 }
-
