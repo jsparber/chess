aaeec0375dcf1c1a901026bd0d6801f840a645ec
fs <unknown-user@192.168.247.129>
2016-03-02 07:10:40 -0800

    + rewrited pawn comments

--- Chess/figures/Pawn.cs
+++ Chess/figures/Pawn.cs
@@ -5,7 +5,11 @@ namespace Chess
   public class Pawn : Figure
 	{
     /* Definition of the variable count used for the check of the movement of the pawn */
+    /* legends: count = 0 pawn not moved;count = 1 pawn moved the first time of one tile; */
+    /* count = 2 pawn has moved the first time of two tiles(it can be )*/
+
     private int count;
+
     /* Constructor of the object pawn*/
     public Pawn (string color) : base(color)
     {
@@ -14,117 +18,48 @@ namespace Chess
 
 
     public override bool move(Board board, coord start, coord end) {
-      //TODO counter for movement; movement of 2 tiles
-      /* Counter TODO TODO TODO  IL PEDONE NON PUÒ ASSOLUTAMENTE SALTERE LE ALTRE PEDINE */
-     /* int count = 0;              
-      int doublemov = 0;          /* Count for checking if the pawn has done the doublemove */ 
+      //TODO mangiare in diagonale e impedire di mangiare dritti
 
- 
-       
+      /* If the color is white we have an increment of y;if the color is black we have a decrement of y. */
+      /* this method return the value "true" if the movement is permitted */
 
-       
-        
-        /* If the color is black we have a decrement of the value y */
-     //   else {
-
-         
-
-          /* Setting count to two */
-       //   this.count = 2;
-
-          /* If the value of x and y are not corresponding to the rule the movement isn't allowed  */
-       //   return false;/***********************************/
-
-    //  }
-        /*Setting doublemov to one 
-        doublemov = 1;*/
-
-      
-    //  }
-      /*-----------------------------------------------------------------*/
-        
-      /* Basic movement of pawn: one tile ahead */
-
-      /* This method check the color of the figure, if it is white*/
-      /* the final position is equal to the initial position with an increment */
-      /* of one of the value y,else the final position is equal to the initial position */
-      /* with a decrement of one of the value y */
-
-      if (this.Color == "white") {    /* Check of the color of the figure */
-
-       /*???????????????????????????????????????????????????????????*/
-      if(this.count == 0){
-          /* Special movement of pawn: two tile ahead */
-
-          /* the final position is equal to the initial position with an increment */
-          /* of two of the value y,else the final position is equal to the initial position */
-          /* with a decrement of two of the value y */
-
-        /* If the final value of y is equal to the starting value plus two */
-        /* there is a control for the value x:if the final value is equal */
-        /* to the starting value the movement is possible */
-        if (start.y + 2 == end.y) {  
-          if (start.x == end.x) {
-            /* Setting count to two */
-            this.count = 2;
-
+      if (this.Color == "white") {      /* Check of the color of the figure,case of a white figure */       
+      if(this.count == 0) {             /* If count is zero the figure has never moved and can move of two tiles ahead */         
+        if (start.y + 2 == end.y) {     /* If final position is equal to initial position with a vertical increment of two */
+          if (start.x == end.x) {       /* and there is the same x position the movement is allowed */       
+              this.count = 2;           /* Setting count to two */
             return true;
           }
         }
-      }
-
-        /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
-
-        /* If the final value of y is equal to the starting value plus one */
-        /* there is a control for the value x:if the final value is equal */
-        /* to the starting value the movement is possible */
-        if (start.y + 1 == end.y) {   
-          if (start.x == end.x) {
-            /* If count is zero it will be set to one */        
-            if(this.count == 0){             
+      }      
+        if (start.y + 1 == end.y) {     /* Basic movement:1 tile ahead */ 
+          if (start.x == end.x) {                
+            if(this.count == 0) {       /* If count is zero it will be set to one */           
               this.count = 1;
             }
             return true;
-
           }
-        }
-
-        /* If the value of x and y are not corresponding to the rule the movement isn't allowed  */
-        return false;
+        }       
+        return false;                   /* If the value of x and y are not corresponding to the rule the movement isn't allowed  */
       } 
-      /* If the color is black we have a decrement of the value y */
-      else {
-      /*????????????????????????????????????????????????????????????????????*/
-      if(this.count == 0){
-
-        /* If the final value of y is equal to the starting value minus two */
-        /* there is a control for the value x:if the final value is equal */
-        /* to the starting value the movement is possible */
-        if (start.y - 2 == end.y) {
-          if (start.x == end.x) {
-            /* Setting count to two */
-            this.count = 2;
+      else {                            /* case of a black figure */      
+        if(this.count == 0) {           /* If count is zero the figure has never moved and can move of two tiles ahead */     
+          if (start.y - 2 == end.y) {   /* If final position is equal to initial position with a vertical decrement of two */
+            if (start.x == end.x) {     /* and there is the same x position the movement is allowed */     
+              this.count = 2;           /* Setting count to two */
             return true;
           }
         }
-      }
-      /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!e*/
-
-        /* If the final value of y is equal to the starting value minus one */
-        /* there is a control for the value x:if the final value is equal */
-        /* to the starting value the movement is possible */
-        if (start.y - 1 == end.y) {
-          if (start.x == end.x) {
-            /* If count is zero it will be set to one */        
-            if (this.count == 0) {             
+      }      
+        if (start.y - 1 == end.y) {     /* Basic movement:1 tile ahead */
+          if (start.x == end.x) {             
+            if (this.count == 0) {      /* If count is zero it will be set to one */               
               this.count = 1;
             }
             return true;
           }
         }
-
-        /* If the value of x and y are not corresponding to the rule the movement isn't allowed  */
-        return false;
+        return false;                   /* If the value of x and y are not corresponding to the rule the movement isn't allowed  */
       }         
     }
 	}
