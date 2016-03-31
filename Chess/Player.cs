1f3977e10be2751fe853e4920fa39bde8b97b786
fs <unknown-user@192.168.247.129>
2016-03-31 05:45:12 -0700

    + added comments

--- Chess/Player.cs
+++ Chess/Player.cs
@@ -2,24 +2,28 @@
 
 namespace Chess
 {
+  /* Class for the player */
   public class Player
   {
-    private string currentPlayer;
+    private string currentPlayer;         /* String identifiyng color of actual player */
 
+    /* Constructor of the player object */
     public Player (string player)
     {
       this.currentPlayer = player;
     }
 
+    /* Override of 'ToString' method */
     public override string ToString ()
     {
-      return currentPlayer;
+      return currentPlayer;                /* It returns the string that represent color of actual player */
     }
 
+    /* Method for the alternance of players */
     public Player next ()
     {
+      /* If current player is white next will be black and vice-versa */
       return new Player ((this.currentPlayer == "white") ? "black" : "white");
     }
   }
 }
-
