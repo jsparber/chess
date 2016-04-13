7be58bee13754feb0f5045ddd12dcee84c08f91b
Julian Sparber <julian@sparber.net>
2016-04-13 12:15:30 +0200

    [feat] add relative path for finding images in the bin dir and some [minor] changes

--- Chess/GUI/TileWidget.cs
+++ Chess/GUI/TileWidget.cs
@@ -3,29 +3,34 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using Gtk;
+
 namespace Chess
 {
   [System.ComponentModel.ToolboxItem (false)]
   public partial class TileWidget : Gtk.EventBox
   {
     public coord position { get; set; }
+
     public string color { get; set; }
+
     public string colorBg { get; set; }
+
     public string figure { get; set; }
+
     public TileWidget (string colorBg, string figure, string color, coord size)
     {
       string imgName;
-      if (color != "" && figure != "" && figure.ToLower() != "empty") {
-        if (figure.ToLower() != "knight") {
+      if (color != "" && figure != "" && figure.ToLower () != "empty") {
+        if (figure.ToLower () != "knight") {
           imgName = color.ToLower () [0].ToString () + figure.ToUpper () [0].ToString ();
         } else {
           imgName = color.ToLower () [0].ToString () + "N";
         }
-        Image img = loadSvg(imgName, size);
+        Image img = loadSvg (imgName, size);
         Fixed f = new Fixed ();
         f.Add (img);
-        f.ShowAll();
-        this.Add(f);
+        f.ShowAll ();
+        this.Add (f);
         this.Show ();
       }
       if (colorBg != "") {
@@ -33,27 +38,29 @@ namespace Chess
         Gdk.Color.Parse (colorBg, ref col);
         this.ModifyBg (StateType.Normal, col);
       }
-        this.figure = figure;
-        this.color = color;
-        this.colorBg = colorBg;
+      this.figure = figure;
+      this.color = color;
+      this.colorBg = colorBg;
     }
 
     public Gtk.Image loadSvg (string file, coord size)
     {
       Gdk.Pixbuf display;
+      string basePath = System.IO.Path.GetDirectoryName (System.Reflection.Assembly.GetExecutingAssembly ().GetName ().CodeBase).Substring (5);
       try {
-        display = new Gdk.Pixbuf ("../../img/cburnett/" + file + ".svg", size.x, size.y);
-      }
-      catch (GLib.GException e) {
+        display = new Gdk.Pixbuf (basePath + @"/img/cburnett/" + file + ".svg", size.x, size.y);
+      } catch (GLib.GException e) {
         Console.WriteLine (e);
         display = null;
       }
       return (new Gtk.Image (display));
 
     }
+
     public Gtk.Image loadCircle (coord size)
     {
-      Gdk.Pixbuf display = new Gdk.Pixbuf ("../../img/circle.svg", size.x, size.y);
+      string basePath = System.IO.Path.GetDirectoryName (System.Reflection.Assembly.GetExecutingAssembly ().GetName ().CodeBase).Substring (5);
+      Gdk.Pixbuf display = new Gdk.Pixbuf (basePath + @"/img/circle.svg", size.x, size.y);
       return (new Gtk.Image (display));
     }
   }
